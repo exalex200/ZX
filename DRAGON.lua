17b501dfdcaa718d84849d34ddd7b3cf6f06d2ab
نايك احمد عياد <83564699+exalex200@users.noreply.github.com>
2021-06-13 17:04:04 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -2683,7 +2683,7 @@ keyboard = {}
 keyboard.inline_keyboard = {
 {{text = '☆ 𝐄𝐋𝐊𝐇𝐀𝐖𝐀𝐉𝐀𝐇 ☆ ',url="t.me/ELkhawajah"}},
 {{text = '☆ 𝐁𝐀𝐑𝐋𝐎𝟎𝐎 ☆',url="t.me/bar_lo0o0"}},
-{{text = '☁️ 𝕊𝕆𝕌ℝℂ𝔼 𝔼𝕃𝕂ℍ𝔸𝕎𝔸𝕁𝔸ℍ ☁️', url="t.me/BARLO0Obot"}},
+{{text = '☁️ 𝕊𝕆𝕌ℝℂ𝔼 𝔼𝕃𝕂ℍ𝔸𝕎𝔸𝕁𝔸ℍ ☁️', url="t.me/ELkhawajah1"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
@@ -9599,6 +9599,33 @@ database:set(bot_id..'Text:Sudo:Bot'..msg.sender_user_id_..':'..msg.chat_id_, te
 database:sadd(bot_id..'List:Rd:Sudo', text)
 return false end
 end
+
+if text == "بوت" or text == 'البوت' then
+local Namebot = (database:get(bot_id..'Name:Bot') or 'اليكس')
+local DRAGON_Msg = {
+'اسمي  '..Namebot..' يا قلبي 🤤💚',
+'اسمي '..Namebot..' يا روحي🙈❤️',
+'اسمي  '..Namebot..' يعمري🌚🌹',
+'اسمي  '..Namebot..' يا قمر 🐭🤍',
+'اسمي  '..Namebot..' يامزه 🥺❤️',
+'اسمي  '..Namebot..' يعم 😒',
+'مقولت اسمي '..Namebot..' في اي 🙄',
+'اسمي الكيوت '..Namebot..' 🌝💘',
+'اسمي  '..Namebot..' ياحياتي🧸♥️',
+'اسمي  '..Namebot..' يوتكه🙈🍑',
+}
+
+Namebot = DRAGON_Msg[math.random(#DRAGON_Msg)]
+local function getpro(extra, result, success)
+if result.photos_[0] then
+sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, result.photos_[0].sizes_[1].photo_.persistent_id_,Namebot, msg.id_, msg.id_, "md")
+else
+send(msg.chat_id_, msg.id_,Namebot, 1, 'md')
+end
+end
+tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = bot_id, offset_ = 0, limit_ = 1 }, getpro, nil)
+end
+
 if text and text:match("^(.*)$") then
 if database:get(bot_id..'Set:On'..msg.sender_user_id_..':'..msg.chat_id_) == 'true' then
 send(msg.chat_id_, msg.id_,' 𖠪 تم ازالة الرد العام')
